0d3ce84125fad6a0a20466d90bbb8f753f66beab
Spencer Buja <chbuja@microsoft.com>
2018-06-29 14:49:32 -0700

    added new files

---
+++ EM.py
@@ -0,0 +1,35 @@
+import numpy as np   
+import scipy.stats
+
+K = 100 #num pixels
+T = 50 #number of training examples
+J = 20 #number of images in library
+
+#hidden parameters
+M = np.random.randint(2,size=(T,K)) # binary r.v.s
+b = np.random.randint(J,size=T)
+f = np.random.randint(J,size=T)
+
+#theta
+alpha = np.random.random(size=(T,K))
+mu = np.random.random(size=(J,K))*255
+psi = np.random.random(size=(J,K))*30
+pi = np.random.random(size=J); pi = pi / sum()
+
+#calling normal density
+scipy.stats.norm(np.array([[100,100,100],[99,99,99]]), [[12,12,12],[10,10,10]]).pdf([98,99,100]) #returns 2x3 matrix
+
+z = np.zeros((T,K))
+
+def EM(M, b, f, alpha, mu, psi, pi, z ):
+    Q = np.zeros((T,J,J))
+    for t in range(T):
+        Q[t,:,:] = np.outer(pi,pi)
+        normal = (scipy.stats.norm(mu, psi).pdf(z[t,:]))  #J x K
+
+        
+
+
+        
+
+        np.outer
---
+++ icm.py
@@ -0,0 +1,47 @@
+import numpy as np   
+import scipy.stats
+
+K = 100 #num pixels
+T = 50 #number of training examples
+J = 20 #number of images in library
+
+#hidden parameters
+M = np.random.randint(2,size=(T,K)) # binary r.v.s
+b = np.random.randint(J,size=T)
+f = np.random.randint(J,size=T)
+
+#theta
+alpha = np.random.random(size=(T,K))
+mu = np.random.random(size=(J,K))*255
+psi = np.random.random(size=(J,K))*30
+pi = np.random.random(size=J); pi = pi / sum()
+
+#calling normal density
+scipy.stats.norm(np.array([[100,100],[99,99]]), [[12,12],[10,10]]).pdf([98,99]) #returns 2x2 matrix
+
+z = np.zeros((T,K))
+
+def icm(M, b, f, alpha, mu, psi, pi, z ):
+    for t in np.range(T):
+        m = M[t,:]
+        f[t] = np.argmax(pi * np.prod((alpha[t,:]**m)*((1-alpha[t,:])**(1-m))*(scipy.stats.norm(mu, psi).pdf(z[t,:])**m), axis=1))
+        
+        mu_f = mu[f[t],:]
+        psi_f = psi[f[t],:]
+        alpha_f = alpha[t,:]
+        
+        M[t,:] = np.argmax(np.array([
+            (1-alpha_f)*scipy.stats.norm(mu[b[t],:],psi[b[t],:]).pdf(z[t,:]),
+            alpha_f*scipy.stats.norm(mu_f, psi_f).pdf(z[t,:])
+        ]), axis=1)
+
+        b[t] = np.argmax(pi * np.prod(( scipy.stats.norm(mu, psi).pdf(z[t,:]) ** (1-M[t,:]) ),axis=1) )
+
+    for j in range(J):
+        pi[j] = (1.0/2*T )*np.sum((f==j).astype(int) + (b==j).astype(int))
+
+    for j in range(J):
+        for i in range(K):
+            alpha[j,i] = np.sum((f == j).astype(float) * M[:,i])/np.sum((f==j).astype(float))
+            mu[j,i] =   np.sum((f == j).astype(float) * (b == j).astype(float) * z[:i]) / np.sum((f == j).astype(float) * (b == j).astype(float) )
+            psi[j,i] = np.sum((f == j).astype(float) * (b == j).astype(float) * (z[:i] - mu[j,i])**2) / np.sum((f == j).astype(float) * (b == j).astype(float) )
